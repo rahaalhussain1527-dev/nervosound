2461e78f4debf01932070164ddee5081b2a02443
rahaalhussain1527-dev <raha.alhussain1527@gmail.com>
2025-12-21 15:33:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -81,7 +81,7 @@
     ctx2d.stroke();
 
     // تدريجات Y (Hz)
-    ctx2d.fillStyle = "#c9d6e7";
+    ctx2d.fillStyle = "#e5e7eb";
     ctx2d.font = "14px system-ui";
 
     const ticks = [0, 40, 80, 200, 500, 1000, 1500, 2000].filter(t => t <= Y_MAX_HZ);
