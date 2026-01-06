f944e9780c035504905c738b3e6dc3bb4cd89866
rahaalhussain1527-dev <raha.alhussain1527@gmail.com>
2026-01-06 17:37:30 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,14 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔥 Firebase configuration (from your console)
 const firebaseConfig = {
-  apiKey: "AIzaSyCvD00QzwL6tmz6Bcz_J8fPF5xkGxIGmwk",
+  apiKey: "AIzaSyCvDO0QZwL6tmz6Bcz_J8FPF5xkGxIGmwk",
   authDomain: "nervosound-a1e73.firebaseapp.com",
   projectId: "nervosound-a1e73",
   storageBucket: "nervosound-a1e73.appspot.com",
   messagingSenderId: "104739152232",
-  appId: "1:104739152232:web:3a29bdec9a38a7b82f36dd",
-  measurementId: "G-W7TRN9WJ85"
+  appId: "1:104739152232:web:3a29bdec9a38a7b82f36dd"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
-// Firestore DB
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
